9b0c02b68baf7248341d62d474598cc718a99031
Evgeniy Saprykin <saprykin.ei@yandex.ru>
2024-12-03 12:25:56 +0300

    Не получается наследоваться через супер(если наследуемся от нескольких классов) Ну а так всё работает

--- LabWork/task_2.py
+++ LabWork/task_2.py
@@ -1,7 +1,43 @@
-class Car:
+class Wheels():
+    '''Колёса автомобиля'''
+
+    def __init__(self, brand_wheels, diameter_wheels):
+        self.brand_wheels = brand_wheels
+        self.diameter_wheels = diameter_wheels
+
+    def get_information_wheels(self):
+        return (f"Бренд колёс: {self.brand_wheels}\n"
+                f"Диаметр колёс: {self.diameter_wheels}\n")
+
+
+class Engine():
+    '''Модель двигателя'''
+
+    def __init__(self, model_engine, engine_power):
+        self.model_engine = model_engine
+        self.engine_power = engine_power
+
+    def get_information_engine(self):
+        return (f"Модель двигателя: {self.model_engine}\n"
+                f"Мощность двигателя: {self.engine_power}\n")
+
+
+class Doors():
+
+    def __init__(self, number_of_doors):
+        self.number_of_doors = number_of_doors
+    
+    def get_information_doors(self):
+        return (f"Количество дверей: {self.number_of_doors}")
+
+
+class Car(Wheels, Engine, Doors):
     '''Модель Автомобиля'''
 
-    def __init__(self, brand, model, year):
+    def __init__(self, brand_wheels, diameter_wheels, model_engine, engine_power, number_of_doors, brand, model, year):
+        super().__init__(brand_wheels, diameter_wheels)
+        Engine.__init__(self, model_engine, engine_power)
+        Doors.__init__(self, number_of_doors)
         self.brand = brand
         self.model = model
         self.year = year
@@ -9,57 +45,22 @@ class Car:
     def get_information_car(self):
         return (f"Автомобиль: {self.brand}\n"
                 f"Модель: {self.model}\n"
-                f"Год выпуска: {self.year}\n")
+                f"Год выпуска: {self.year}\n"
+                f"{super().get_information_wheels()}"
+                f"{super().get_information_engine()}"
+                f"{super().get_information_doors()}")
 
 
-class Wheels(Car):
-    '''Колёса автомобиля'''
-
-    def __init__(self, brand, model, year, brand_wheels, diameter_wheels):
-        super().__init__(brand, model, year)
-        self.brand_wheels = brand_wheels
-        self.diameter_wheels = diameter_wheels
-
-    def get_information_car(self):
-        data_car = super().get_information_car()
-        return (f"{data_car}")
-
-    def get_inform_wheels(self):
-        return (f"Бренд колёс: {self.brand_wheels}\n "
-                f"Диаметр колёс: {self.diameter_wheels}\n")
-
-
-class Engine(Car, Wheels):
-    '''Модель двигателя'''
-
-    def __init__(self, brand, model, year, brand_wheels, diameter_wheels, model_engine, engine_power):
-        super().__init__(brand, model, year, brand_wheels, diameter_wheels)
-        self.model_engine = model_engine
-        self.engine_power = engine_power
-
-    def get_information_car(self):
-        data_car = super().get_information_car()
-        return (f"{data_car}")
-
-    def get_inform_wheels(self):
-        data_wheels = super().get_inform_wheels()
-        return data_wheels
-
-    def get_inform_engine(self):
-        return (f"Модель двигателя: {self.model_engine}\n"
-                f"Мощность двигателя: {self.engine_power}\n")
-#
-#
-#
-car_bmw = Car('BMW', 'X5-M', '2022')
+car_bmw = Car('Michelin',
+              24,
+              'B57D30',
+              '286 л.с',
+              '5',
+              'BMW',
+              'X5-M',
+              2020)
 print(car_bmw.get_information_car())
 
-car_bmw = Wheels('BMW', 'X5-M', 2022, 'Michelin', 24)
-print(car_bmw.get_inform_wheels())
 
-car_bmw = Engine('BMW', 'X5-M', 2022, 'Michelin', 24, 'B57D30 ', '286 л.с')
-print(car_bmw.get_information_car())
-print(car_bmw.get_inform_engine())
-print(car_bmw.get_inform_engine())
 
 
