3b70d87c39c22ef1655be4a61efb1064284d6fb7
Evgeniy Saprykin <saprykin.ei@yandex.ru>
2024-12-21 10:01:44 +0300

    тесты

---
+++ tests/Pytest/test's/main.py
@@ -0,0 +1,114 @@
+class Teacher:
+    '''Модель преподавателя'''
+    teacher_dict = {}
+
+    def __init__(self, name, education, experience, discipline):
+        self.__name = name
+        self.__education = education
+        self.__experience = experience
+        self.__discipline = discipline
+        Teacher.teacher_dict[self.__name] = [self.__education,
+                                             self.__experience,
+                                             self.__discipline]
+
+    def get_teacher_name(self):
+        return self.__name
+
+    def get_education(self):
+        return self.__education
+
+    def get_experience(self):
+        return self.__experience
+
+    def set_experience(self, experience):
+        self.__experience = experience
+
+    def get_discipline(self):
+        return self.__discipline
+
+    def get_teacher_data(self):
+        return f"{self.get_teacher_name()}, образование {self.get_education()}, опыт работы {self.get_experience()} года"
+
+    def add_mark(self, student_name, estimation):
+        return f"{self.get_teacher_name()} поставил оценку {estimation} студенту {student_name}"
+
+    def remove_mark(self, student_name, estimation):
+        return f"{self.get_teacher_name()}, удалил оценку {estimation} студенту {student_name}"
+
+    def give_a_consultation(self, student_class):
+        return f"{self.get_teacher_name()} провел консультацию в классе {student_class}"
+
+    def fire_teacher(self):
+        if self.__name in Teacher.teacher_dict:
+            Teacher.teacher_dict.pop(self.__name)
+            return f"Сотрудник {self.__name} был уволен"
+        else:
+            return f"Сотрудника {self.__name} уже уволили"
+
+
+class DisciplineTeacher(Teacher):
+    "Модель Преподавателя по предмету"
+    discipline_teacher_dict = {}
+
+    def __init__(self, name, education, experience, discipline, job_title):
+        super().__init__(name, education, experience, discipline)
+        self.__job_title = job_title
+        DisciplineTeacher.discipline_teacher_dict[name] = [education,
+                                                           experience,
+                                                           discipline,
+                                                           self.__job_title]
+
+
+
+    def get_discipline(self):
+        return super().get_discipline()
+
+    def get_job_title(self):
+        return self.__job_title
+
+    def set_job_title(self, job_title):
+        self.__job_title = job_title
+
+    def get_teacher_data(self):
+        data = super().get_teacher_data()
+        return f"{data},\nПредмет {Teacher.get_discipline(self)}, должность {self.__job_title}\n"
+
+
+    def add_mark(self, student_name, estimation):
+        data = super().add_mark(student_name, estimation)
+        return f"{data}.\nПредмет {self.get_discipline()}\n"
+
+    def remove_mark(self, student_name, estimation):
+        super().remove_mark(student_name, estimation)
+        return (f"{self.get_teacher_name()}, удалил оценку {estimation} студенту {student_name}.\n"
+                f"Предмет {self.get_discipline()}\n")
+
+    def give_a_consultation(self, student_class):
+        super().give_a_consultation(student_class)
+        return (f"{self.get_teacher_name()} провел консультацию в классе {student_class}.\n"
+                f"По предмету {self.get_discipline()} как {self.__job_title}\n")
+
+
+
+
+
+
+teach = Teacher('Иван Петров', 'БГПУ', 4, None)
+print(teach.get_teacher_data())
+print(teach.add_mark('Петр Сидоров', 4))
+print(teach.remove_mark('Дмитрий Степанов', 3))
+print(teach.give_a_consultation('9Б'))
+teach.set_experience(5)
+print(teach.get_teacher_data())
+print()
+
+
+director = DisciplineTeacher("Иван Петров", "БГПУ", 4, "Химия", "Директор")
+print(director.get_teacher_data())
+print(director.add_mark("Николай Иванов", 4))
+print(director.remove_mark("Дмитрий Сидоров", 3))
+print(director.give_a_consultation("10 Б"))
+print()
+director.set_job_title("Учитель")
+print(director.get_teacher_data())
+print(director.give_a_consultation("10 Б"))
---
+++ tests/Pytest/test's/test_02_disciplineTeacher.py
@@ -0,0 +1,38 @@
+
+
+from main import DisciplineTeacher
+
+def test_01_init(discipline_teacher_fixture):
+    assert len(DisciplineTeacher.discipline_teacher_dict) == 1
+    assert DisciplineTeacher.discipline_teacher_dict == {'Иван Петров': ["БГПУ", 4, "Химия", "Директор"]}
+
+
+def test_02_get_discipline(discipline_teacher_fixture):
+    assert discipline_teacher_fixture.get_discipline() == 'Химия'
+
+
+def test_03_get_job_title(discipline_teacher_fixture):
+    assert discipline_teacher_fixture.get_job_title() == 'Директор'
+
+
+def test_04_set_job_tittle(discipline_teacher_fixture):
+    discipline_teacher_fixture.set_job_title('Учитель')
+    assert discipline_teacher_fixture.get_job_title() == 'Учитель'
+
+def test_05_get_teacher_data(discipline_teacher_fixture):
+    assert discipline_teacher_fixture.get_teacher_data() == ('Иван Петров, образование БГПУ, опыт работы 4 года,'
+                                                             '\nПредмет Химия, должность Директор\n')
+
+
+def test_06_add_mark(discipline_teacher_fixture):
+    assert discipline_teacher_fixture.add_mark("Николай Иванов", 4) == ('Иван Петров поставил оценку 4 студенту Николай Иванов.\n'
+                                                                          'Предмет Химия\n')
+
+
+def test_07_remove_mark(discipline_teacher_fixture):
+    assert discipline_teacher_fixture.remove_mark("Дмитрий Сидоров", 3) == 'Иван Петров, удалил оценку 3 студенту Дмитрий Сидоров.\nПредмет Химия\n'
+
+
+def test_08_give_a_consultation(discipline_teacher_fixture):
+    assert discipline_teacher_fixture.give_a_consultation('10 Б') == ('Иван Петров провел консультацию в классе 10 Б.\n'
+                                                                      'По предмету Химия как Директор\n')
